9e101cc4b2bb984544664cf5c35ee066b820f6f8
“Matthew <3017703P@student.gla.ac.uk>
2026-03-01 17:53:10 +0000

    added list of categories on main page

--- rango/admin.py
+++ rango/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
-admin.site.register(Category)
-
+admin.site.register(Category, CategoryAdmin)
 
 class pageAdmin(admin.ModelAdmin):
     list_display = ('title','category', 'url')
