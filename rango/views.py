ae419ed48ad809a307e5a1a4d01876484e66ce9f
“Matthew <3017703P@student.gla.ac.uk>
2026-03-01 19:51:12 +0000

    added most viewed pages and passed tests

--- rango/views.py
+++ rango/views.py
@@ -5,9 +5,10 @@ from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-    context_dict['categories'] = category_list
+    pages = Page.objects.order_by('-views')[:5]
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+                    'categories': category_list,
+                    'pages': pages}
     return render(request, 'rango/index.html', context=context_dict)
 
 
@@ -18,11 +19,11 @@ def show_category(request, category_name_slug):
     context_dict = {}
 
     try:
-        Category = Category.objects.get(slug=category_name_slug)
-        pages = Page.objects.filter(Category=Category)
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category).order_by('title')
 
+        context_dict['category'] = category
         context_dict['pages'] = pages
-        context_dict['category'] = Category
 
     except Category.DoesNotExist:
         context_dict['category'] = None
