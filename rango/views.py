5f73693812a0569f1da97f6da519713231d70e30
“Matthew <3017703P@student.gla.ac.uk>
2026-03-03 16:30:46 +0000

    added sign up access to register page, created full login functionality

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from rango.models import Category
 from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
@@ -121,3 +122,25 @@ def register(request):
             'registered': registered,
         }
     )
+
+
+def user_login(request):
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        return render(request, 'rango/login.html')
